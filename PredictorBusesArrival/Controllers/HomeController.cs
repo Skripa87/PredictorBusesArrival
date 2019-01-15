cc542e9387362c3dc393be54676817d66de71afb
Alex <skripinalexey1987@gmail.com>
2019-01-15 17:59:49 +0500

    Класс

--- PredictorBusesArrival/Controllers/HomeController.cs
+++ PredictorBusesArrival/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PredictorBusesArrival.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,11 @@ namespace PredictorBusesArrival.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            PredictorManager manager = new PredictorManager();
+            await manager.GetAllStations("http://glonass.ufagortrans.ru/php/getStations.php?city=ufagortrans&info=12345&_=1517558480807");
             return View();
-        }        
+        } 
     }
 }
---
+++ PredictorBusesArrival/Models/PredictorManager.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PredictorBusesArrival.Models
+{
+    public class PredictorManager
+    {
+        static HttpClient httpClient;
+
+        public PredictorManager()
+        {
+            httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("http://glonass.ufagortrans.ru/php/")
+            };
+            httpClient.DefaultRequestHeaders
+                      .Accept
+                      .Clear();
+            httpClient.DefaultRequestHeaders
+                      .Accept
+                      .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> GetAllStations(string path)
+        {
+            string result = null;
+            HttpResponseMessage response = await httpClient.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+            }
+            var spliterResult = new List<string>();
+            result.Split("},{".ToArray());
+            var jResult = JToken.Parse(result).ToObject<IEnumerable<Station>>();
+            return result;
+        }
+    }
+}
